7f4cdcf300719a66acbdedd6d97829cde4fa0e8f
raj nayak <155231280+rajrnayakwork@users.noreply.github.com>
2024-06-20 19:10:45 +0530

    changes in home and cup list screen.

--- app/pages/cup-list/CupListScreen.jsx
+++ app/pages/cup-list/CupListScreen.jsx
@@ -1,9 +1,47 @@
-import { Text } from "react-native-ui-lib";
+import { Picker, SegmentedControl, Text, View } from "react-native-ui-lib";
+import { useState } from "react";
+
+{
+	/* Vendor, Entry At, Total Cups, Total Amount, Remark, Created By,
+		{
+			Product, Cups, Price, Amount
+		} */
+}
 
 const CupListScreen = () => {
+	let vendors = [
+		{ id: 1, name: "AnkitTea" },
+		{ id: 2, name: "BabuLal" },
+		{ id: 3, name: "ChiragTea" },
+	];
+	const [filterDetails, setFilterDetails] = useState({
+		vendor: 0,
+		product: 0,
+	});
+
 	return (
 		<>
-			<Text>Cup-List Page.</Text>
+			<View flex>
+				<View flex-1 center row margin-10 backgroundColor="#fff">
+					<View flex-2>
+						<Text text50BO>Orders List</Text>
+					</View>
+					<View flex-2>
+						<SegmentedControl backgroundColor="white" activeColor="#5AB2FF" segmentLabelStyle={{ width: 40, textAlign: "center" }} segments={[{ label: "Month" }, { label: "Year" }, { label: "All" }]} />
+					</View>
+				</View>
+				<View flex-1 center row marginR-10 marginL-10 marginB-10 backgroundColor="#fff">
+					<View flex-2>
+						<Picker value={filterDetails.vendor} placeholder={"Vendors"} onChange={(value) => setFilterDetails({ ...filterDetails, vendor: value })}>
+							{vendors.map((vendor, index) => (
+								<Picker.Item key={index} value={vendor.id} label={vendor.name} />
+							))}
+						</Picker>
+					</View>
+					<View flex-2></View>
+				</View>
+				<View flex-10 marginR-10 marginL-10 backgroundColor="#fff"></View>
+			</View>
 		</>
 	);
 };
--- app/pages/home/HomeScreen.jsx
+++ app/pages/home/HomeScreen.jsx
@@ -1,20 +1,88 @@
-import { Text, View } from "react-native-ui-lib";
+import { Card, Chip, GridList, SegmentedControl, Text, View } from "react-native-ui-lib";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const HomeScreen = () => {
+	let cups = [
+		{ vendorName: "AnkitBha", date: "2 May 2024", tea: 2, coffee: 3 },
+		{ vendorName: "AnkitBhi", date: "3 May 2024", tea: 3, coffee: 4 },
+		{ vendorName: "AnkitBai", date: "4 May 2024", tea: 4, coffee: 5 },
+		{ vendorName: "AnkitBha", date: "2 May 2024", tea: 2, coffee: 3 },
+		{ vendorName: "AnkitBhi", date: "3 May 2024", tea: 3, coffee: 4 },
+		{ vendorName: "AnkitBai", date: "4 May 2024", tea: 4, coffee: 5 },
+		{ vendorName: "AnkitBha", date: "2 May 2024", tea: 2, coffee: 3 },
+		{ vendorName: "AnkitBhi", date: "3 May 2024", tea: 3, coffee: 4 },
+		{ vendorName: "AnkitBai", date: "4 May 2024", tea: 4, coffee: 5 },
+	];
+
+	const CardDetails = ({ icon, text, value, amountIcon }) => {
+		return (
+			<>
+				<View flex center padding-20>
+					<View flex-1 row center gap-10>
+						<Ionicons name={icon} size={32} color="#5AB2FF" />
+						<Text>{text}</Text>
+					</View>
+					<View flex-1 center>
+						<Text text30>{amountIcon ? "₹" + value : value}</Text>
+					</View>
+				</View>
+			</>
+		);
+	};
+
 	return (
 		<>
-			<View backgroundColor="blue" flex>
-				<View backgroundColor="red" flex-1>
-					<Text>Row 1</Text>
+			<View flex>
+				<View flex-1 paddingL-10 paddingR-10>
+					<View flex-1 row center>
+						<View flex-1 left>
+							<Text text50>Overview</Text>
+						</View>
+						<View flex-2 right>
+							<SegmentedControl backgroundColor="white" segmentLabelStyle={{ width: 40, textAlign: "center" }} activeColor="#5AB2FF" segments={[{ label: "Week" }, { label: "Month" }]} />
+						</View>
+					</View>
+					<View flex-2 row gap-10>
+						<Card flex backgroundColor="white">
+							<CardDetails icon="cafe-outline" text="Total Orders" value="500" />
+						</Card>
+						<Card flex center backgroundColor="white">
+							<CardDetails icon="cash-outline" text="Total Amounts" value="5000" amountIcon />
+						</Card>
+					</View>
 				</View>
-				<View backgroundColor="orange" flex-3>
-					<Text>Row 2</Text>
-				</View>
-				<View backgroundColor="purple" flex-1>
-					<Text>Row 3</Text>
-				</View>
-				<View backgroundColor="green" flex-6>
-					<Text>Row 4</Text>
+				<View flex-2>
+					<View flex-1 row center paddingL-10 paddingR-10>
+						<View flex-1 left>
+							<Text text50>Cup List</Text>
+						</View>
+						<View flex-2 right>
+							<Text>
+								<Chip label={"Last 7 Days Details"} />
+							</Text>
+						</View>
+					</View>
+					<View flex-6>
+						<GridList
+							listPadding={5}
+							data={cups}
+							numColumns={1}
+							itemSpacing={2}
+							renderItem={({ item }) => (
+								<Card backgroundColor="white" padding-6 paddingL-10 paddingR-10 margin-4>
+									<Card.Section
+										content={[
+											{ text: item.date, style: { position: "absolute", top: 10, right: 10 } },
+											{ text: item.vendorName, text50: true },
+											{ text: `tea - ${item.tea}`, text70: true },
+											{ text: `Coffee - ${item.coffee}`, text70: true },
+											{ text: <Ionicons name="chevron-forward-outline" size={20} />, style: { position: "absolute", right: 10, bottom: 10 } },
+										]}
+									/>
+								</Card>
+							)}
+						/>
+					</View>
 				</View>
 			</View>
 		</>
