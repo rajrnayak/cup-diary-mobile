fdbf4cff4367c0fcf5b2bbb877a0655436837deb
raj nayak <155231280+rajrnayakwork@users.noreply.github.com>
2024-06-21 14:35:11 +0530

    Update HomeScreen.jsx

--- app/pages/home/HomeScreen.jsx
+++ app/pages/home/HomeScreen.jsx
@@ -1,20 +1,20 @@
-import { Card, Chip, GridList, SegmentedControl, Text, View } from "react-native-ui-lib";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
+import { Card, Chip, Dialog, GridList, Modal, SegmentedControl, Text, View } from "react-native-ui-lib";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 const HomeScreen = () => {
 	let cups = [
-		{ vendorName: "AnkitBha", date: "2 May 2024", tea: 2, coffee: 3 },
-		{ vendorName: "AnkitBhi", date: "3 May 2024", tea: 3, coffee: 4 },
-		{ vendorName: "AnkitBai", date: "4 May 2024", tea: 4, coffee: 5 },
-		{ vendorName: "AnkitBha", date: "2 May 2024", tea: 2, coffee: 3 },
-		{ vendorName: "AnkitBhi", date: "3 May 2024", tea: 3, coffee: 4 },
-		{ vendorName: "AnkitBai", date: "4 May 2024", tea: 4, coffee: 5 },
-		{ vendorName: "AnkitBha", date: "2 May 2024", tea: 2, coffee: 3 },
-		{ vendorName: "AnkitBhi", date: "3 May 2024", tea: 3, coffee: 4 },
-		{ vendorName: "AnkitBai", date: "4 May 2024", tea: 4, coffee: 5 },
+		// Vendor, Entry At, Total Cups, Total Amount, Remark, Created By
+		{ vendor_name: "Ankit Bhai", entry_at: "2 May 2024", total_cups: 2, total_amount: 300, remark: "Nothing", user_name: "Raj" },
 	];
 
-	const CardDetails = ({ icon, text, value, amountIcon }) => {
+	const dialogBoxRef = useRef(null);
+
+	const openDialogBox = (cupList) => {
+		dialogBoxRef.current.open(cupList);
+	};
+
+	const OverviewCardDetails = ({ icon, text, value, amountIcon }) => {
 		return (
 			<>
 				<View flex center padding-20>
@@ -24,6 +24,38 @@ const HomeScreen = () => {
 					</View>
 					<View flex-1 center>
 						<Text text30>{amountIcon ? "₹" + value : value}</Text>
+					</View>
+				</View>
+			</>
+		);
+	};
+
+	const CupListCardDetails = ({ cupList }) => {
+		return (
+			<>
+				<View flex row padding-5>
+					<View flex-6 gap-5>
+						<Text text50BO color="#00A9FF">
+							{cupList.vendor_name}
+						</Text>
+						<Text>{cupList.entry_at}</Text>
+					</View>
+					<View flex-5 gap-5>
+						<View flex row gap-5 center>
+							<Text>Total Cups :</Text>
+							<Text text70BO color="#00A9FF">
+								{cupList.total_cups}
+							</Text>
+						</View>
+						<View flex row gap-5 center>
+							<Text>Total Amount :</Text>
+							<Text text70BO color="#00A9FF">
+								{cupList.total_amount}
+							</Text>
+						</View>
+					</View>
+					<View flex-1 center>
+						<Ionicons name="chevron-forward-outline" size={20} />
 					</View>
 				</View>
 			</>
@@ -44,10 +76,10 @@ const HomeScreen = () => {
 					</View>
 					<View flex-2 row gap-10>
 						<Card flex backgroundColor="white">
-							<CardDetails icon="cafe-outline" text="Total Orders" value="500" />
+							<OverviewCardDetails icon="cafe-outline" text="Total Orders" value="500" />
 						</Card>
 						<Card flex center backgroundColor="white">
-							<CardDetails icon="cash-outline" text="Total Amounts" value="5000" amountIcon />
+							<OverviewCardDetails icon="cash-outline" text="Total Amounts" value="5000" amountIcon />
 						</Card>
 					</View>
 				</View>
@@ -69,24 +101,92 @@ const HomeScreen = () => {
 							numColumns={1}
 							itemSpacing={2}
 							renderItem={({ item }) => (
-								<Card backgroundColor="white" padding-6 paddingL-10 paddingR-10 margin-4>
-									<Card.Section
-										content={[
-											{ text: item.date, style: { position: "absolute", top: 10, right: 10 } },
-											{ text: item.vendorName, text50: true },
-											{ text: `tea - ${item.tea}`, text70: true },
-											{ text: `Coffee - ${item.coffee}`, text70: true },
-											{ text: <Ionicons name="chevron-forward-outline" size={20} />, style: { position: "absolute", right: 10, bottom: 10 } },
-										]}
-									/>
+								<Card backgroundColor="white" padding-6 paddingL-10 paddingR-10 margin-4 onPress={() => openDialogBox(item)}>
+									<CupListCardDetails cupList={item} />
 								</Card>
 							)}
 						/>
 					</View>
 				</View>
 			</View>
+			<CupListDialogBox ref={dialogBoxRef} />
 		</>
 	);
 };
+
+const CupListDialogBox = forwardRef((props, ref) => {
+	const [isVisible, setIsVisible] = useState(false);
+
+	const cupListData = useRef({});
+
+	const open = (cupList) => {
+		cupListData.current = cupList;
+		setIsVisible(true);
+	};
+
+	const close = () => {
+		setIsVisible(false);
+		cupListData.current = {};
+	};
+
+	useImperativeHandle(
+		ref,
+		() => {
+			return {
+				open,
+				close,
+			};
+		},
+		[]
+	);
+
+	return (
+		<>
+			<Modal visible={isVisible} onBackgroundPress={() => close()} animationType="slide" onRequestClose={() => close()} transparent>
+				<View flex bottom margin-10>
+					<Card padding-20 onPress={() => close()}>
+						<View gap-10>
+							<View center paddingB-10>
+								<Text text40BO color="#00A9FF">
+									Cup-List Details
+								</Text>
+							</View>
+							<View row>
+								<Text flex>Vendor Name : </Text>
+								<Text flex text60BO>
+									{cupListData.current.vendor_name}
+								</Text>
+							</View>
+							<View row>
+								<Text flex>Entry At : </Text>
+								<Text flex text70BO>
+									{cupListData.current.entry_at}
+								</Text>
+							</View>
+							<View row>
+								<Text flex>User Name : </Text>
+								<Text flex text70BO>
+									{cupListData.current.user_name}
+								</Text>
+							</View>
+							<View row>
+								<Text flex>Total Cups : </Text>
+								<Text flex text70BO>
+									{cupListData.current.total_cups}
+								</Text>
+							</View>
+							<View row>
+								<Text flex>Total Amount : </Text>
+								<Text flex text70BO>
+									{cupListData.current.total_amount}
+								</Text>
+							</View>
+						</View>
+					</Card>
+				</View>
+			</Modal>
+		</>
+	);
+});
 
 export default HomeScreen;
