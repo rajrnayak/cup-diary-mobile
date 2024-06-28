9c213b6bb39957056687f77c0b2449595c64cc7c
Mahir Sherasiya <sherasiyamahir24@gmail.com>
2024-06-28 11:43:18 +0530

    payment module changes

---
+++ app/pages/payment/PaymentModal.jsx
@@ -0,0 +1,88 @@
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
+import { Card, Modal, Text, View } from "react-native-ui-lib";
+import Divider from "../../component/Divider";
+
+const PaymentModal = forwardRef((props, ref) => {
+	const [isVisible, setIsVisible] = useState(false);
+
+	const paymentData = useRef({});
+
+	const open = (payment) => {
+		paymentData.current = payment;
+		setIsVisible(true);
+	};
+
+	const close = () => {
+		setIsVisible(false);
+		paymentData.current = {};
+	};
+
+	useImperativeHandle(
+		ref,
+		() => {
+			return {
+				open,
+				close,
+			};
+		},
+		[]
+	);
+
+	return (
+		<>
+			<Modal visible={isVisible} onBackgroundPress={() => close()} animationType="slide" onRequestClose={() => close()} overlayBackgroundColor="rgba(0,0,0,0.6)" transparent>
+				<View flex bottom margin-10>
+					<Card blurOptions padding-20 onPress={() => close()}>
+						<View gap-5>
+							<View center paddingB-10>
+								<Text text40BO color="#00A9FF" marginB-10>
+									Payment Details
+								</Text>
+							</View>
+							<View row>
+								<View flex center>
+									<Text>Vendor</Text>
+									<Text text60BO>{paymentData.current.vendors && paymentData.current.vendors.name}</Text>
+								</View>
+								<View flex center>
+									<Text>Created By</Text>
+									<Text text60BO>{paymentData.current.users && paymentData.current.users.username}</Text>
+								</View>
+							</View>
+							<Divider />
+							<View row>
+								<View flex center>
+									<Text>Type</Text>
+									<Text text60BO>{paymentData.current.type === 0 ? 'Credit' : 'Debit'}</Text>
+								</View>
+								<View flex center>
+									<Text>Amount</Text>
+									<Text text60BO>{paymentData.current.amount}</Text>
+								</View>
+							</View>
+							<Divider />
+							<View row>
+								<Text flex center>
+									Payment At :
+								</Text>
+								<Text flex text70BO>
+									{paymentData.current.payment_at}
+								</Text>
+							</View>
+							<View row>
+								<Text flex center>
+									Remark :
+								</Text>
+								<Text flex text70BO>
+									{paymentData.current.remark ? paymentData.current.remark : "-"}
+								</Text>
+							</View>
+						</View>
+					</Card>
+				</View>
+			</Modal>
+		</>
+	);
+});
+
+export default PaymentModal;
